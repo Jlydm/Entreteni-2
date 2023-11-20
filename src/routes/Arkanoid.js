107fdfc6fc9a0ba7b91690e634538e2139dd534d
ElJuanxi <111474574+ElJuanxi@users.noreply.github.com>
2023-11-20 12:54:50 -0500

    Update Arkanoid.js

--- src/routes/Arkanoid.js
+++ src/routes/Arkanoid.js
@@ -14,9 +14,7 @@ const Arkanoid = () => {
             className='rounded-3'
             />
             <div className="guide">
-                <img src='arrows.png' />
-                <img src='arrows.png' />
-                <img src='arrows.png' />
+                <img src='mouse.png' />
             </div>
             <div className="description">
                 <p>
