3799b82eba3692a894c14ac7d8e2c6456904673b
ElJuanxi <111474574+ElJuanxi@users.noreply.github.com>
2023-11-20 19:25:56 -0500

    Update Tanquesitos.js

--- src/routes/Tanquesitos.js
+++ src/routes/Tanquesitos.js
@@ -10,7 +10,7 @@ const Tanquesitos = () => {
             width="1200px"
             height="800px"
             allowFullScreen
-            className='rounded-3'
+            className='rounded-3  clip'
             />
             <div className="guide">
                 <img src='arrows.png' />
