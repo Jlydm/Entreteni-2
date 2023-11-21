f728cfa6c532312a4095fde6c3b9633ff18c407a
ElJuanxi <111474574+ElJuanxi@users.noreply.github.com>
2023-11-20 20:14:51 -0500

    Update Rubik.js

--- src/routes/Rubik.js
+++ src/routes/Rubik.js
@@ -11,7 +11,7 @@ const Rubik = () => {
             width="1200px"
             height="800px"
             allowFullScreen
-            className='rounded-3 clip'
+            className='rounded-3 clips'
             />
             <div className="guide">
                 <img src='mouse.png' />
