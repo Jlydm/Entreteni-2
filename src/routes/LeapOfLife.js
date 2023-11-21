b0d6df80181d6369eef2a2981cb833cdc8e43664
ElJuanxi <111474574+ElJuanxi@users.noreply.github.com>
2023-11-20 19:29:14 -0500

    Update LeapOfLife.js

--- src/routes/LeapOfLife.js
+++ src/routes/LeapOfLife.js
@@ -11,7 +11,7 @@ const LoL = () => {
             width="1200px"
             height="800px"
             allowFullScreen
-            className='rounded-3 clip'
+            className='rounded-3'
             />
             <div className="guide">
                 <img src='arrows.png' />
